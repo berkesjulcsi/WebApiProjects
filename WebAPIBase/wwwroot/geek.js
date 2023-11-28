19e17e98b5de6b764d0b2b7ddb1def65be19d8c4
berkesjulcsi <julia.berkes@stud.uni-corvinus.hu>
2023-11-28 10:44:25 +0100

    befejezve

---
+++ WebAPIBase/wwwroot/geek.js
@@ -0,0 +1,25 @@
+﻿var viccek;
+
+window.onload = function () {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+    );
+
+    function letöltésBefejeződött(d) {
+        console.log("Sikeres letöltés")
+        console.log(d)
+        viccek = d;
+
+        for (var i = 0; i < viccek.length; i++) {
+
+        var UjDiv = document.createElement("div");
+        UjDiv.innerText = viccek[i].question;
+        document.getElementById("vicc").appendChild(UjDiv);
+    }
+    }
+    
+
+    
+
+};
---
+++ WebAPIBase/wwwroot/scripts/jsgyak.js
@@ -0,0 +1,23 @@
+﻿window.onload = () => {
+    console.log("betöltődött")
+    for (var sor = 0; sor < 10; sor++) {
+        var ujDIv = document.createElement("div");
+        ujDIv.classList.add('sor');
+        var pascalDiv = document.getElementById("pascal");
+        pascalDiv.appendChild(ujDIv);
+
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            var ujElemDIv = document.createElement("div");
+            ujElemDIv.classList.add('elem');
+            ujElemDIv.innerText = faktoriális(sor) / (faktoriális(oszlop) * faktoriális(sor-oszlop));
+            ujDIv.appendChild(ujElemDIv);
+        }
+    }
+};
+var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
